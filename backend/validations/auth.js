b4e2d3e0a0b1a39647093d70a667b59c18ebb40e
Illya <illakoroluk62@gmail.com>
2025-03-20 22:01:55 +0200

    fix syntax error. auth.js

--- backend/validations/auth.js
+++ backend/validations/auth.js
@@ -4,5 +4,5 @@ export const registerValidaton = [
     body('email', 'не вірний формат почти').isEmail(),
     body('password', 'пароль повинен містити мінімум 5 символів ').isLength({min: 6}),
     body('fullName', `вкажіть ім'я`).isLength({min: 3}),
-    body('avatarUrl', 'не вірне посилання на аватар').opptional().isURL()
+    body('avatarUrl', 'не вірне посилання на аватар').optional().isURL()
 ]
