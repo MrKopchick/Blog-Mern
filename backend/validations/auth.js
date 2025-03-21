ead2a5f4ac402fc6345bf7bc04cffe2d9fa155b6
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-21 15:03:30 +0200

    remove auth.js

--- backend/validations/auth.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-
-export const registerValidaton = [
-    body('email', 'не вірний формат почти').isEmail(),
-    body('password', 'пароль повинен містити мінімум 5 символів ').isLength({min: 6}),
-    body('fullName', `вкажіть ім'я`).isLength({min: 3}),
-    body('avatarUrl', 'не вірне посилання на аватар').optional().isURL()
-]
