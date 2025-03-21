be135699905be18c3797cad1b03e6f55018754d2
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-21 21:26:06 +0200

    refact validations logic UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,35 +1,31 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { check, validationResult } from 'express-validator';
 import UserModel from './models/user.js';
+
 
 export const register = async (req, res) => {
     try{
-        const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }
+        
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    
-    const document = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash: hash
-    });
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        
+        const document = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash
+        });
 
-    const user = await document.save();
-    const token = jwt.sign({id: user._id}, 'secret123', { expiresIn: '30d'});
+        const user = await document.save();
+        const token = jwt.sign({id: user._id}, 'secret123', { expiresIn: '30d'});
 
-    const {passwordHash, ...userData} = user._doc;
-    res.json({
-        ...userData,
-        token
-    })
-
+        const {passwordHash, ...userData} = user._doc;
+        res.json({
+            ...userData,
+            token
+        });
     }catch(err){
         res.status(500).json({
             message: 'не вдалося зареєструватися. Щось не так з введеними даними',
