ab9144dfbbea09dc19c3372670508e570c9e659f
MrKopchick <145700600+MrKopchick@users.noreply.github.com>
2025-03-21 15:08:48 +0200

    add post vallidation

---
+++ backend/validations/postValidations.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator';
+
+export const registerValidaton = [
+    body('email', 'не вірний формат почти').isEmail(),
+    body('password', 'пароль повинен містити мінімум 5 символів ').isLength({min: 6}),
+    body('fullName', `вкажіть ім'я`).isLength({min: 3}),
+    body('avatarUrl', 'не вірне посилання на аватар').optional().isURL()
+]
