67ade68d2d8a4be32cc3b323f2ab0a5cee8a5398
Illya <illakoroluk62@gmail.com>
2025-03-23 13:39:32 +0200

    add some logics in redux

--- frontend/src/redux/slices/posts.js
+++ frontend/src/redux/slices/posts.js
@@ -1,4 +1,15 @@
-import { createSlice} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const {data} = await axios.get('/posts');
+    return data;
+})
+
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+    const {data} = await axios.get('/tags');
+    return data;
+})
 
 const initialState = {
     posts: {
@@ -11,10 +22,25 @@ const initialState = {
     }
 };
 
+
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {},
+    reducers: {},
+    extraReducers: {    
+        [fetchPosts.pending]: (state, actions) =>{
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, actions) => {
+            state.posts.items = actions.payload;
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items= [];
+            state.posts.status = 'error';   
+        }
+    }
 });
 
 export const postReducer = postsSlice.reducer;
