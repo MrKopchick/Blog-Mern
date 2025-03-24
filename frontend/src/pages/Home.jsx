b826dec1f10a91f8ccf56381a2bf5bf7291369c3
Illya <illakoroluk62@gmail.com>
2025-03-24 17:27:28 +0200

    complete home and fullpost pages. fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,17 +9,19 @@ import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
 
 import {useDispatch, useSelector} from 'react-redux';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 export const Home = () => {
   
   const dispatch = useDispatch();
-  const {posts, tags } = useSelector((state) => state.posts);
+  const {posts, tags } = useSelector(state => state.posts);
 
   const isPostLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';  
 
+  console.log(tags.items);
   React.useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, 
   []);
 
@@ -31,15 +33,14 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostLoading ?  
-          (<Post key = {index} isLoading={true}/>
-
+          {(isPostLoading ? [...Array(5)] : posts.items).map((obj, index) => 
+          isPostLoading ? (
+            <Post key = {index} isLoading={true}/>
           ):(
-
             <Post 
               id = {obj._id}
               title = {obj.title}
-              imageUrl={"https://i.pinimg.com/736x/6c/33/5a/6c335ad992327aa54ba030e01674d8a4.jpg"}
+              imageUrl={obj.imageUrl}
               user= {obj.user}
               createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
@@ -49,9 +50,10 @@ export const Home = () => {
             />
           ) )}
         </Grid>
+        
         <Grid xs={4} item>
-          <TagsBlock items={tags} isLoading={isTagsLoading} />
-          <CommentsBlock
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          {/*<CommentsBlock
             items={[
               {
                 user: {
@@ -66,10 +68,10 @@ export const Home = () => {
                   avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
                 },
                 text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
-              },
+              },    
             ]}
             isLoading={false}
-          />
+          />*/}
         </Grid>
       </Grid>
     </>
