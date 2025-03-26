71965a6aca7d092e9d5dddd81ef37412fa47368e
Illya <illakoroluk62@gmail.com>
2025-03-26 18:29:11 +0200

    adding the right to edit posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,6 +14,7 @@ export const Home = () => {
   
   const dispatch = useDispatch();
   const {posts, tags } = useSelector(state => state.posts);
+  const userData = useSelector((state) => state.auth.data); 
 
   const isPostLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';  
@@ -46,7 +47,7 @@ export const Home = () => {
               viewsCount={obj.viewsCount}
               commentsCount={3}
               tags={[obj.tags]}
-              isEditable
+              isEditable = {userData?._id === obj.user._id}
             />
           ) )}
         </Grid>
